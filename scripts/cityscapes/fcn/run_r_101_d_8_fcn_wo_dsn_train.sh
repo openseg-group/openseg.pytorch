f508e23e728dcb1e45ed63e109abfc2db8dfd746
hsfzxjy <hsfzxjy@163.com>
2020-04-14 15:29:56 +0800

    fix phase

--- scripts/cityscapes/fcn/run_r_101_d_8_fcn_wo_dsn_train.sh
+++ scripts/cityscapes/fcn/run_r_101_d_8_fcn_wo_dsn_train.sh
@@ -30,7 +30,7 @@ MAX_ITERS=40000
 if [ "$1"x == "train"x ]; then
   ${PYTHON} -u main.py --configs ${CONFIGS} \
                        --drop_last y \
-                       --phase train_clean \
+                       --phase train \
                        --gathered n \
                        --loss_balance y \
                        --log_to_file n \
@@ -48,7 +48,7 @@ if [ "$1"x == "train"x ]; then
 elif [ "$1"x == "resume"x ]; then
   ${PYTHON} -u main.py --configs ${CONFIGS} \
                        --drop_last y \
-                       --phase train_clean \
+                       --phase train \
                        --gathered n \
                        --loss_balance y \
                        --log_to_file n \
